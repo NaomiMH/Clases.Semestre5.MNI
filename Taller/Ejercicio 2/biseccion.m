f18acf14ebd8b0053396c23aa2bf689f49f31916
Naomi M. Honti <naomiperla.macias@hotmail.com>
2018-09-19 17:38:58 -0500

    Programa

--- Taller/Ejercicio 2/biseccion.m
+++ Taller/Ejercicio 2/biseccion.m
@@ -10,27 +10,27 @@ Xi = limites(1);
 Xu = limites(2);
 continuar = true;
 while(continuar)
-iteraciones = input('Ingrese el escalar de iteraciones maximas: ');
-contador = 1;
-Ea = 0;
-varargout{2}=[];
-while(contador <= iteraciones)
-    Xr = (Xi+Xu)/2;
-    if(contador>1)
-        Ea = abs((Xr-Ea)/Xr)*100;
+    iteraciones = input('Ingrese el escalar de iteraciones maximas: ');
+    contador = 1;
+    Ea = 0;
+    varargout{2}=[];
+    while(contador <= iteraciones)
+        Xr = (Xi+Xu)/2;
+        if(contador>1)
+            Ea = abs((Xr-Ea)/Xr)*100;
+        end
+        varargout{2} = [[Xi;Xu;Ea] varargout{2}];
+        if(round(handle(Xr),4)== 0)
+            varargout{1} = [Xr;Ea];
+            contador = iteraciones;
+        elseif(handle(Xr)*handle(Xi) < 0)
+            Xu = Xr;
+        elseif(handle(Xr)*handle(Xi) > 0)
+            Xi = Xr;
+        end
+        Ea = Xr;
+        contador=contador+1;
     end
-    varargout{2} = [[Xi;Xu;Ea] varargout{2}];
-    if(round(handle(Xr),4)== 0)
-        varargout{1} = [Xr;Ea];
-        contador = iteraciones;
-    elseif(handle(Xr)*handle(Xi) < 0)
-        Xu = Xr;
-    elseif(handle(Xr)*handle(Xi) > 0)
-        Xi = Xr;
-    end
-    Ea = Xr;
-    contador=contador+1;
-end
-continuar = input('Desea refinar? [true/1 o false/0] ');
+    continuar = input('Desea refinar? [true/1 o false/0] ');
 end
 end
